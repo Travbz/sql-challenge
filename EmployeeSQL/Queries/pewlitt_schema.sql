88f47a35b6bd63f0ce56ba95548cc0eada6c16f7
Travbz <78450169+Travbz@users.noreply.github.com>
2021-05-06 19:56:46 -0600

    final query

--- EmployeeSQL/Queries/pewlitt_schema.sql
+++ EmployeeSQL/Queries/pewlitt_schema.sql
@@ -1,65 +1,81 @@
-DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS salaries;
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS titles;
+SELECT * FROM employees;
+SELECT * FROM salaries;
 
--- Create table for title data
-CREATE TABLE titles (
-	title_id varchar(5) not null,
-	title varchar(255),
-	PRIMARY KEY (title_id)
-)
+
+--Q1 List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	e.sex,
+	s.salary
+from employees e
+inner join salaries s on 
+e.emp_no = s.emp_no;
+
+--Q2 List first name, last name, and hire date for employees who were hired in 1986.
+SELECT
+first_name,
+last_name,
+hire_date
+FROM employees
+WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-31';
+
+--Q3 List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT
+dm.dept_no,
+dm.emp_no,
+d.dept_name
+into dept_config
+from dept_manager dm
+inner join departments d on
+dm.dept_no = d.dept_no;
+	
+SELECT 
+e.first_name,
+e.last_name,
+dc.dept_no,
+dc.emp_no,
+dc.dept_name
+from employees e
+right join dept_config dc on
+e.emp_no = dc.emp_no;
+
+--Q4 . List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT
+	e.first_name,
+	e.last_name,
+	de.emp_no,
+	d.dept_name
+into employee_clean_question_4
+from dept_emp de
+inner join employees e on e.emp_no = de.emp_no
+inner join departments d on d.dept_no = de.dept_no;
+
+SELECT * FROM employee_clean_question_4;
+
+--Q5 List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT 
+	e.first_name,
+	e.last_name,
+	e.sex
+FROM employees e
+WHERE first_name ='Hercules'
+	AND last_name like 'B%'
 ;
 
--- Create table for employee data
-CREATE TABLE employees (
-	emp_no integer not null,
-	emp_title varchar(5) not null,
-	birth_date date not null,
-	first_name varchar(255) not null,
-	last_name varchar(255) not null,
-	sex varchar(1) not null,
-	hire_date date not null,
-	PRIMARY KEY (emp_no),
-	FOREIGN KEY (emp_title) REFERENCES titles (title_id)
-)
-;
 
--- Create table for department data
-CREATE TABLE departments (
-	dept_no varchar(4) not null,
-	dept_name varchar(255) not null,
-	PRIMARY KEY (dept_no)
-)
-;
+--Q6 List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT * FROM employee_clean_question_4
+WHERE dept_name = 'Sales';
 
--- Create table for salary data
-CREATE TABLE salaries (
-	emp_no integer not null,
-	salary integer not null,
-	PRIMARY KEY (emp_no, salary),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-)
-;
+--Q7 List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT * FROM employee_clean_question_4
+WHERE dept_name = 'Sales' or dept_name = 'Development';
 
--- Create table for dept_emp data
-CREATE TABLE dept_emp (
-	emp_no integer not null,
-	dept_no varchar(4) not null,
-	PRIMARY KEY (emp_no, dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
-)
-;
-
--- Create table for dept_manager data
-CREATE TABLE dept_manager (
-	dept_no varchar(4) not null,
-	emp_no integer not null,
-	PRIMARY KEY (dept_no, emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-)
-;
+--Q8 In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT
+last_name, COUNT(last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT DESC;
